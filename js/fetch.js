a1b3d25c4ec267d12f71d7a093f9cfc777d711ad
Axel Piñeros <axelpineros@outlook.com>
2023-10-12 20:56:19 +0200

    Primer fetch correcte

--- js/fetch.js
+++ js/fetch.js
@@ -1,23 +1,40 @@
 (function loadData() {
 	fetch('https://rickandmortyapi.com/api/character/')
+
 		.then(response => {
 			if (!response.ok) {
 				throw new Error("Error HTTP: " + response.status);
 			}
 			return response.json();
 		})
+
 		.then(data => {
 			const characters = data.results;
-			let output = '';
+			let content = '';
 			characters.forEach(character => {
-				output += `<li>Nombre: ${character.name}, Especie: ${character.species}, Estado: ${character.status}</li>, Imagen: ${character.image}`;
+				let type = '';
+				if (character.type !== "") {
+					type = `<li><strong>Type:</strong> ${character.type}</li>`;
+				}
+				content +=
+				`<article>
+					<h2>${character.name}</h2>
+					<img src="${character.image}" alt="Character ${character.name}">
+					<ul>
+						<li><strong>Species:</strong> ${character.species}</li>
+						${type}
+						<li><strong>Status:</strong> ${character.status}</li>
+						<li><strong>Gender:</strong> ${character.gender}</li>
+						<li><strong>Origin:</strong> ${character.origin.name}</li>
+						<li><strong>Location:</strong> ${character.location.name}</li>
+					</ul>
+				</article>`;
 			});
-			document.getElementById('characters').innerHTML = output;
+			document.getElementById('characters').innerHTML = content;
 		})
-		.catch(error => {
-			console.error('Error:', error);
-			document.getElementById('characters').innerHTML = "Error: " + error.message;
+
+		.catch(__error => {
+			// console.error('Error:', error);
+			document.getElementById('characters').innerHTML = "We're sorry, there's been an error on our side";
 		});
 })();
-
-// document.querySelector("button").onclick = loadData;
