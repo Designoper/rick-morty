716b58f913e111abcfd6ea4bfc0589b5368525fe
Axel Piñeros <axelpineros@outlook.com>
2023-10-13 20:03:10 +0200

    Integrat buscador

--- js/fetch.js
+++ js/fetch.js
@@ -1,10 +1,70 @@
+const characterUrl = `https://rickandmortyapi.com/api/character/`;
+// const search = document.querySelector('input').value;
+// const query = `?name=${search}`;
+
 function loadData(url) {
 
-	fetch(url)
+	fetch(`${url}`)
 
 		.then(response => {
 			if (!response.ok) {
 				throw new Error("Error HTTP: " + response.status);
+			}
+			return response.json();
+		})
+
+		.then(data => {
+			const characters = data.results;
+			let content = '';
+			characters.forEach(character => {
+				let type = '';
+				if (character.type !== "") {
+					type = `<li><strong>Type:</strong> ${character.type}</li>`;
+				}
+				content +=
+					`<article>
+						<h2>${character.name}</h2>
+						<img src="${character.image}" alt="Character ${character.name}">
+						<ul>
+							<li><strong>Species:</strong> ${character.species}</li>
+							${type}
+							<li><strong>Status:</strong> ${character.status}</li>
+							<li><strong>Gender:</strong> ${character.gender}</li>
+							<li><strong>Origin:</strong> ${character.origin.name}</li>
+							<li><strong>Location:</strong> ${character.location.name}</li>
+						</ul>
+					</article>`;
+			});
+			document.getElementById('characters').innerHTML += content;
+			nextPage(data.info.next);
+		})
+
+		.catch(error => {
+			console.error('Error:', error);
+			// document.getElementById('characters').innerHTML += "We're sorry, there's been an error on our side";
+		});
+};
+
+window.onload = loadData(characterUrl);
+
+function nextPage(param) {
+	document.querySelector('button').onclick = function () {
+		loadData(param);
+	}
+}
+
+
+
+
+
+function prova() {
+
+	const input = `${baseUrl}?name=${document.getElementById('input').value}`;
+	fetch(input)
+
+		.then(response => {
+			if (!response.ok) {
+				document.getElementById('characters').innerHTML = 'Sorry, no results found';
 			}
 			return response.json();
 		})
@@ -31,37 +91,18 @@ function loadData(url) {
 					</ul>
 				</article>`;
 			});
-			document.getElementById('characters').innerHTML += content;
-			// document.querySelector('button').onclick = function () {
-			// 	loadData(data.info.next);
-			// }
+			document.getElementById('characters').innerHTML = content;
 			nextPage(data.info.next);
-
 		})
 
 		.catch(error => {
 			console.error('Error:', error);
 			// document.getElementById('characters').innerHTML += "We're sorry, there's been an error on our side";
 		});
-};
-
-window.onload = loadData('https://rickandmortyapi.com/api/character/');
-// document.querySelector("button").addEventListener('click', function () {
-// 	loadData('https://rickandmortyapi.com/api/character/');
-// });
-
-function nextPage(param) {
-	document.querySelector('button').onclick = function () {
-		loadData(param);
-	}
 }
+document.getElementById('input').oninput = prova;
 
-// window.onscroll = function() {
-//     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-//         // Cargar más datos
-//         loadData('url').then(next => {
-//             // Usar la variable next
-//             console.log(next);
-//         });
-//     }
-// };
+// }
+
+// const input = document.getElementById('input').innerHTML;
+// document.getElementById('input').addEventListener('input', loadData(input));
