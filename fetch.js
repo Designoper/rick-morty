d72d630cae626eb184ab1f9f3352f2de07b702ea
Axel Piñeros <axelpineros@outlook.com>
2023-10-14 13:14:03 +0200

    Simplificació significativa codi

--- fetch.js
+++ fetch.js
@@ -1,93 +1,36 @@
-const characterEndpoint = `https://rickandmortyapi.com/api/character/`;
-const queryParameter = `?name=`;
+const characterEndpoint = `https://rickandmortyapi.com/api/character/?name=`;
 const nextPageButton = document.querySelector('#next-page');
 const apiContent = document.querySelector('#characters');
 const errorMessage = document.querySelector('#error');
 const userInput = document.querySelector('#input');
 
-function loadData(request) {
+const loadCharacters = (endpoint, __param) => {
 
-	fetch(`${request}`)
+	if (__param === undefined) {
+		__param = '';
+	}
+	const request = `${endpoint}${__param}`;
 
-		.then(response => {
-			if (!response.ok) {
-				throw new Error("Error HTTP: " + response.status);
-			}
-			// document.getElementById('error').innerHTML = 'no  characters';
-			return response.json();
-		})
-
-		.then(data => {
-			const characters = data.results;
-			let content = '';
-			characters.forEach(character => {
-				let type;
-				character.type === '' ? type = '' : type = `<li><strong>Type:</strong> ${character.type}</li>`;
-				content +=
-					`<article>
-						<h2>${character.name}</h2>
-						<img src="${character.image}" alt="Character ${character.name}">
-						<ul>
-							<li><strong>Species:</strong> ${character.species}</li>
-							${type}
-							<li><strong>Status:</strong> ${character.status}</li>
-							<li><strong>Gender:</strong> ${character.gender}</li>
-							<li><strong>Origin:</strong> ${character.origin.name}</li>
-							<li><strong>Location:</strong> ${character.location.name}</li>
-						</ul>
-					</article>`;
-			});
-			apiContent.innerHTML += content;
-			nextPage(data.info.next);
-		})
-
-		.catch(error => {
-			console.error('Error:', error);
-			//  document.getElementById('characters').innerHTML += "We're sorry, there's been an error on our side";
-		});
-};
-
-window.onload = loadData(characterEndpoint);
-
-function nextPage(request) {
-	nextPageButton.onclick = () => request === null ? errorMessage.innerHTML = 'No more characters' : loadData(request);
-}
-
-
-
-
-
-function searchUpdate() {
-
-	const request = `${characterEndpoint}${queryParameter}${userInput.value}`;
 	fetch(request)
 
 		.then(response => {
-			if (!response.ok) {
-				apiContent.innerHTML = '';
-				errorMessage.innerHTML = 'No results found';
-				nextPageButton.style.visibility = 'hidden';
-			}
-			else {
-				errorMessage.innerHTML = '';
-				nextPageButton.style.visibility = 'visible';
-			}
+			errorMessage.innerHTML = '';
+			nextPageButton.style.visibility = 'visible';
 			return response.json();
 		})
 
 		.then(data => {
 			const characters = data.results;
 			let content = '';
+
 			characters.forEach(character => {
-				let type;
-				character.type === '' ? type = '' : type = `<li><strong>Type:</strong> ${character.type}</li>`;
 				content +=
 					`<article>
 						<h2>${character.name}</h2>
 						<img src="${character.image}" alt="Character ${character.name}">
 						<ul>
 							<li><strong>Species:</strong> ${character.species}</li>
-							${type}
+							${character.type === '' ? '' : `<li><strong>Type:</strong> ${character.type}</li>`}
 							<li><strong>Status:</strong> ${character.status}</li>
 							<li><strong>Gender:</strong> ${character.gender}</li>
 							<li><strong>Origin:</strong> ${character.origin.name}</li>
@@ -95,14 +38,22 @@ function searchUpdate() {
 						</ul>
 					</article>`;
 			});
-			apiContent.innerHTML = content;
-			nextPage(data.info.next);
+			switch (__param) {
+				case (''): apiContent.innerHTML += content;
+					break;
+				default: apiContent.innerHTML = content;
+			}
+			nextPageButton.onclick = () => data.info.next === null ? errorMessage.innerHTML = 'No more characters' : loadCharacters(data.info.next);
 		})
 
 		.catch(error => {
 			console.error('Error:', error);
-			// document.getElementById('characters').innerHTML += "We're sorry, there's been an error on our side";
+			apiContent.innerHTML = '';
+			errorMessage.innerHTML = 'No results found';
+			nextPageButton.style.visibility = 'hidden';
 		});
-}
+};
 
-userInput.oninput = searchUpdate;
+window.onload = loadCharacters(characterEndpoint);
+
+userInput.oninput = () => loadCharacters(characterEndpoint, userInput.value);
