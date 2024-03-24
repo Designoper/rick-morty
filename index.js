9d587b24a26e68e551a0193f5974bdead491a32b
a <a>
2024-03-24 12:28:20 +0100

    Promesa a async await

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
 const API_ENDPOINT = `https://rickandmortyapi.com/api/character/?name=`;
-const FETCH_RESPONSE = document.getElementById('fetchResponse');
+const RESPONSE_CONTAINER = document.getElementById('fetchResponse');
 const SEARCH_BAR = document.getElementById('searchBar');
 const NEXT_PAGE_BUTTON = document.getElementById('nextPageButton');
 
-const fetchApi = async (url) => {
+const mainFunction = async (url) => {
+	const RESPONSE = await fetchResource(url);
+	printCharacters(RESPONSE);
+	updateButton(RESPONSE);
+}
+
+const fetchResource = async (url = API_ENDPOINT) => {
 	try {
 		const RESPONSE = await fetch(url);
-
 		const { results: characters, info: page } = await RESPONSE.json();
+		return { characters, page }
+	} catch (error) {
+		RESPONSE_CONTAINER.textContent = 'Connection failed.';
+	}
+}
 
+const printCharacters = ({ characters }) => {
+	try {
 		const CHARACTER_CARDS = characters.map(character =>
 			`<article>
 				<h2>${character.name}</h2>
 				<img src="${character.image}" alt="Character ${character.name}">
-				<ul>
-					<li><strong>Species:</strong> ${character.species}</li>
-					${character.type === '' ? '' : `<li><strong>Type:</strong> ${character.type}</li>`}
-					<li><strong>Status:</strong> ${character.status}</li>
-					<li><strong>Gender:</strong> ${character.gender}</li>
-					<li><strong>Origin:</strong> ${character.origin.name}</li>
-					<li><strong>Location:</strong> ${character.location.name}</li>
-				</ul>
+					<ul>
+						<li><strong>Species:</strong> ${character.species}</li>
+						${character.type ? `<li><strong>Type:</strong> ${character.type}</li>` : ''}
+						<li><strong>Status:</strong> ${character.status}</li>
+						<li><strong>Gender:</strong> ${character.gender}</li>
+						<li><strong>Origin:</strong> ${character.origin.name}</li>
+						<li><strong>Location:</strong> ${character.location.name}</li>
+					</ul>
 			</article>`
 		).join('');
 
-		FETCH_RESPONSE.insertAdjacentHTML("beforeend", CHARACTER_CARDS);
-
-		if (page.next) {
-			NEXT_PAGE_BUTTON.style.display = 'block';
-			NEXT_PAGE_BUTTON.onclick = () => fetchApi(page.next);
-		} else {
-			NEXT_PAGE_BUTTON.style.display = 'none';
-		}
-
+		RESPONSE_CONTAINER.insertAdjacentHTML("beforeend", CHARACTER_CARDS);
 	} catch (error) {
-		FETCH_RESPONSE.textContent = 'No characters found';
+		RESPONSE_CONTAINER.textContent = 'No characters found.';
 		NEXT_PAGE_BUTTON.style.display = 'none';
 	}
 }
 
-window.onload = () => fetchApi(API_ENDPOINT);
-
-const clean = () => FETCH_RESPONSE.innerHTML = '';
-
-const addUserInput = () => {
-	clean();
-	fetchApi(`${API_ENDPOINT}${SEARCH_BAR.value}`);
+const updateButton = ({ page }) => {
+	NEXT_PAGE_BUTTON.style.display = page.next ? 'block' : 'none';
+	NEXT_PAGE_BUTTON.addEventListener("click", () => mainFunction(page.next));
 }
 
-SEARCH_BAR.oninput = addUserInput;
+const clean = () => RESPONSE_CONTAINER.innerHTML = '';
+
+SEARCH_BAR.addEventListener("input", () => {
+	clean();
+	mainFunction(`${API_ENDPOINT}${SEARCH_BAR.value}`);
+});
+
+window.addEventListener("load", () => mainFunction());
